2d7e455979da4602f51eb2b8f387b02b49628a2d
joer9514 <joer9514@gmail.com>
2020-08-11 21:57:39 +0000

    update models files

--- models/place.py
+++ models/place.py
@@ -18,3 +18,7 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    reviews = relationship("Review", cascade="delete", backref="place")
+    amenities = relationship("Amenity",
+                                 secondary=association_table,
+                                 viewonly=False)
