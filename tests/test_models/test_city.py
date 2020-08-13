f96dbe794efff7ff02bac764374d002544a054fd
santiagopemo <santiagopemo@gmail.com>
2020-08-13 03:26:35 +0000

    added test for user & amenity & city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,40 +1,92 @@
 #!/usr/bin/python3
 """ """
 import unittest
-from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+from models.base_model import BaseModel
 from sqlalchemy.orm.collections import InstrumentedList
+from datetime import datetime
+import models
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+import os
+import pep8
 NoneType = type(None)
 
 
-class test_City(unittest.TestCase):
+class TestCity(unittest.TestCase):
     """ """
 
     @classmethod
     def setUpClass(cls):
-        """test_city testing setup"""
-        pass
+        """executes after all tests"""
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
+
+    @classmethod
+    def tearDownClass(cls):
+        """executes after all tests"""
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
 
     def setUp(self):
         """executes after a test"""
-        self.name = "City"
-        self.value = City
+        pass
 
     def tearDown(self):
         """executes before a test"""
         pass
 
+
     def test_state_id(self):
         """ """
-        new = self.value()
+        new = City()
         self.assertEqual(type(new.state_id), NoneType)
 
     def test_name(self):
         """ """
-        new = self.value()
+        new = City()
         self.assertEqual(type(new.name), NoneType)
 
     def test_places(self):
         """ """
-        new = self.value()
+        new = City()
         self.assertEqual(type(new.places), InstrumentedList)
+
+    def test_pep8(self):
+        """Test pep8 styling."""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(["models/city.py"])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_docstrings(self):
+        """Check for docstrings."""
+        self.assertIsNotNone(City.__doc__)
+
+    def test_instance_attributes(self):
+        """Check for attributes."""
+        ct = City()
+        self.assertTrue(hasattr(ct, "__tablename__"))
+        self.assertTrue(hasattr(ct, "name"))
+        self.assertTrue(hasattr(ct, "state_id"))
+
+    def test_is_subclass_City(self):
+        """Inherits from BaseModel"""
+        ct = City()
+        self.assertTrue(issubclass(ct.__class__, BaseModel), True)
+
+    @unittest.skipIf(type(models.storage) is DBStorage, "Filestorage")
+    def test_save_City(self):
+        """test if the save works"""
+        ct = City()
+        ct.save()
+        self.assertNotEqual(ct.created_at, ct.updated_at)
+
+    def test_to_dict(self):
+        """test to_dict"""
+        ct = City()
+        self.assertTrue('to_dict' in dir(ct))
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,102 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
 from models.user import User
+import unittest
+from models.city import City
+from models.base_model import BaseModel
+from sqlalchemy.orm.collections import InstrumentedList
+from datetime import datetime
+import models
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+import os
+import pep8
+NoneType = type(None)
 
 
-class test_User(test_basemodel):
+class TestUser(unittest.TestCase):
     """ """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+    @classmethod
+    def setUpClass(cls):
+        """executes after all tests"""
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
+
+    @classmethod
+    def tearDownClass(cls):
+        """executes after all tests"""
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+
+    def setUp(self):
+        """executes after a test"""
+        pass
+
+    def tearDown(self):
+        """executes before a test"""
+        pass
 
     def test_first_name(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        new = User()
+        self.assertEqual(type(new.first_name), NoneType)
 
     def test_last_name(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        new = User()
+        self.assertEqual(type(new.last_name), NoneType)
 
     def test_email(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
+        new = User()
+        self.assertEqual(type(new.email), NoneType)
 
     def test_password(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+        new = User()
+        self.assertEqual(type(new.password), NoneType)
+
+    def test_pep8_User(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/user.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_checking_for_docstring_User(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(User.__doc__)
+
+    def test_attributes_User(self):
+        """chekcing if User have attributes"""
+        us = User()
+        self.assertTrue(hasattr(us, "__tablename__"))
+        self.assertTrue(hasattr(us, "email"))
+        self.assertTrue(hasattr(us, "id"))
+        self.assertTrue(hasattr(us, "created_at"))
+        self.assertTrue(hasattr(us, "updated_at"))
+        self.assertTrue(hasattr(us, "password"))
+        self.assertTrue(hasattr(us, "first_name"))
+        self.assertTrue(hasattr(us, "last_name"))
+
+    def test_is_subclass_User(self):
+        """test if User is subclass of Basemodel"""
+        us = User()
+        self.assertTrue(issubclass(us.__class__, BaseModel), True)
+
+    @unittest.skipIf(type(models.storage) is DBStorage, "Filestorage")
+    def test_save_User(self):
+        """test if the save works"""
+        us = User()
+        us.save()
+        self.assertNotEqual(us.created_at, us.updated_at)
+
+    def test_to_dict_User(self):
+        """test if dictionary works"""
+        us = User()
+        self.assertEqual('to_dict' in dir(us), True)
