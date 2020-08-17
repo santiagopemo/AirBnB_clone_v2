0857cd74d902b2cc4ad496b5e0b21d98b25e836b
santiagopemo <santiagopemo@gmail.com>
2020-08-17 23:45:25 +0000

    corrected data/web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,8 +24,9 @@ def do_deploy(archive_path):
                                                         file_name, just_name))
         api.run("rm /tmp/{}".format(file_name))
         path_r = "/data/web_static/releases/"
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path_r, just_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(just_name))
+        api.run('mv {0}{1}/web_static/* {0}{1}/'.format(path_r, just_name))
+        api.run('rm -rf /data/web_static/releases/{}/web_static'.format(
+                                                                just_name))
         api.run("rm -rf /data/web_static/current")
         api.run("ln -s /data/web_static/releases/{} \
                     /data/web_static/current".format(just_name))
