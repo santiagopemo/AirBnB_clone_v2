9a770e2c170b52586c2abab87eab5a4bffb8125b
santiagopemo <santiagopemo@gmail.com>
2020-08-17 20:24:03 +0000

    added task pack web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Pack web static module"""
+from datetime import datetime
+from os import path
+from fabric import api
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of
+    the web_static folder of AirBnB Clone repo
+    """
+    if not path.isdir("versions"):
+        api.local("mkdir versions")
+    date = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    file_name = "web_static_{}.tgz".format(date)
+    result = api.local("tar -czvf {} web_static")
+    if result.failed:
+        return None
+    return file_name
