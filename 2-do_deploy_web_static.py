38c293cc7fbb456f71d542a11f2437726bb1aadf
santiagopemo <santiagopemo@gmail.com>
2020-08-17 22:21:51 +0000

    added do deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Do deploy web static module"""
+from datetime import datetime
+from os import path
+from fabric import api
+
+env.user = 'ubuntu'
+env.hosts = ['35.231.95.4', '54.175.222.196']
+
+
+def do_deploy(archive_path):
+    """
+    Fabric script (based on the file 1-pack_web_static.py) that distributes
+    an archive to your web servers, using the function do_deploy
+    """
+    if not path.exists(archive_path):
+        return False
+    try:
+        file_name = archive_path.split("/")[-1]
+        just_name = file_name.split(".")[0]
+        api.put(archive_path, "/tmp/")
+        api.run("mkdir -p /data/web_static/releases/{}".format(just_name))
+        api.run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+                                                        file_name, just_name))
+        api.run("rm /tmp/{}".format(file_name))
+        api.run("rm -rf /data/web_static/current")
+        api.run("ln -s /data/web_static/releases/{} \
+                    /data/web_static/current".format(just_name))
+        return True
+    except:
+        return False
