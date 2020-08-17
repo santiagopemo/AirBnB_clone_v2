799954252d8b0b467e25058f249f9bc3e55ab3b2
santiagopemo <santiagopemo@gmail.com>
2020-08-17 19:02:06 +0000

    added setup web static script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Write a Bash script that sets up your web servers for the deployment of web_static
 apt-get update -y
-apt-get - y install nginx
+apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton School from /data/web_static/releases/test/index.html" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
-sed -i '/:80 default_server/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sed -i "/:80 default_server/ a \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
